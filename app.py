e58c80c048702c90336dd39c3b9d3bf8cd9d7f99
ankitsuraj <94295922+ankitsuraj@users.noreply.github.com>
2025-07-19 17:20:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import os
-from werkzeug.utils import secure_filename
-import time
 
 app = Flask(__name__)
 CORS(app)
@@ -10,9 +8,11 @@ CORS(app)
 UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 @app.route('/')
 def home():
     return 'SnapBox Backend Server is Running!'
+
 @app.route('/upload', methods=['POST'])
 def upload_files():
     if 'photos' not in request.files:
@@ -21,10 +21,10 @@ def upload_files():
     files = request.files.getlist('photos')
     saved_files = []
 
-    for file in files:
+    for index, file in enumerate(files):
         if file.filename == '':
             continue
-        filename = secure_filename(f"{int(time.time())}-{file.filename}")
+        filename = f"selfie_{index + 1}.jpg"
         path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(path)
         saved_files.append({
@@ -42,5 +42,6 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
+    import time
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
